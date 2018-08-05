66cf30195e737b94fbfd201da55abb54dcbe946c
bodpunk <bodpunk@mail.ru>
2018-08-05 16:20:36 +0500

    First js
    Добавление массива с кодом, позволяющим включать в html рандомные карты на основной экран + рандомное перемешивание этих карт

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var arrCards = ["0C", "0D", "0H", "0S", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S", "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S", "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S", "AC", "AD", "AH", "AS", "JC", "JD", "JH", "JS", "KC", "KD", "KH", "KS", "QC", "QD", "QH", "QS"];
+
+$(document).ready(function() {
+  for (var i = 0; i < 9; i++) {
+    var rand = Math.floor(Math.random() * arrCards.length);
+    var x = arrCards[rand];
+    $(".main-screen").append('<img class="cards__item" src="img/' + x + '.png" width="226" height="314">');
+    $(".main-screen").append('<img class="cards__item" src="img/' + x + '.png" width="226" height="314">');
+    arrCards.splice(rand, 1);
+  };
+  var parent = $(".main-screen");
+    var imgItems = parent.children();
+    while (imgItems.length) {
+        parent.append(imgItems.splice(Math.floor(Math.random() * imgItems.length), 1)[0]);
+    }
+});
